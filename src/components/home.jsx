260fbc97432998778a1c3d7c764bc99804b07756
TanakaAkihiro0930 <redolution0930@gmail.com>
2024-02-28 06:55:21 +0900

    fix

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -21,8 +21,8 @@ const HomePage = (props) => {
                                         <p className="hero__subtitle text-2xl text-center dark:text-white">
                                             The most <span className="text-[#ffb4ed] dark:text-[#FFD6F5]">popular</span> <span className="text-[#6db1ff] dark:text-[#6db1ff]">smart</span> <span className="text-[#FF8F8F]  dark:text-[#FF8F8F]">contracts</span> on <span className="text-[#ffef40] dark:text-[#FFF7A1]">evm</span>.
                                         </p>
-                                        <button onClick={OpenModelPage} class="relative inline-flex items-center px-12 py-3 mt-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50 dark:bg-[rgb(22,22,22)]">
-                                            <span class="absolute left-0 block w-full h-0 transition-all bg-indigo-600 dark:bg-[rgb(31,31,31)] opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+                                        <button onClick={OpenModelPage} class="relative inline-flex items-center px-12 py-3 mt-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 dark:text-white border-indigo-600 hover:text-white dark:border-white rounded-full dark:hover:text-[rgb(22,22,22)] group hover:bg-gray-50 dark:bg-[rgb(22,22,22)]">
+                                            <span class="absolute left-0 block w-full h-0 transition-all bg-indigo-600 dark:bg-white opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
                                             <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
                                                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                                             </span>
